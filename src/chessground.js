b679011f09c790bc2e9a6b0c169692ef3fd1c0a7
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-05-27 14:07:02 +0200

    separate mutable state from constants

--- src/chessground.js
+++ src/chessground.js
@@ -3,30 +3,46 @@
 var Chess = require('chess.js').Chess;
 
 function Chessground(element, cfg) {
-  var chessground = {};
 
   cfg = cfg || {};
-  var files = 'abcdefgh';
-  var types = {p: 'pawn', r: 'rook', b: 'bishop', n: 'knight', q: 'queen', k: 'king'};
-  var colors = {w: 'white', b: 'black'};
-  var startFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-  var chess = new Chess(cfg.fen || startFEN);
+
+  var constants = {
+    files: 'abcdefgh',
+    types: {
+      p: 'pawn',
+      r: 'rook',
+      b: 'bishop',
+      n: 'knight',
+      q: 'queen',
+      k: 'king'
+    },
+    colors: {
+      w: 'white',
+      b: 'black'
+    }
+  };
+
+  // all mutable data goes in there
+  var state = {
+    chess: new Chess(cfg.fen),
+    orientation: cfg.orientation || 'white'
+  };
 
   function drawSquares() {
     var html = '';
     for (var rank = 8; rank > 0; rank--) {
       for (var file = 1; file < 9; file++) {
-        var key = files[file - 1] + rank;
+        var key = constants.files[file - 1] + rank;
         html += '<div data-key="' + key + '"></div>';
       }
     }
     element.innerHTML = html;
   }
 
-  function drawPieces(chess) {
+  var drawPieces = function() {
     Array.prototype.forEach.call(element.children, function(square) {
-      var piece = chess.get(square.getAttribute('data-key'));
-      var html = piece ? '<div class="piece ' + types[piece.type] + ' ' + colors[piece.color] + '"></div>' : '';
+      var piece = state.chess.get(square.getAttribute('data-key'));
+      var html = piece ? '<div class="piece ' + constants.types[piece.type] + ' ' + constants.colors[piece.color] + '"></div>' : '';
       square.innerHTML = html;
     });
   }
@@ -40,12 +56,9 @@ function Chessground(element, cfg) {
   }
 
   drawSquares();
-  drawPieces(chess);
+  drawPieces();
 
-  // public members
-  chessground.position = position;
-
-  return chessground;
+  return {};
 }
 
 module.exports = Chessground;
