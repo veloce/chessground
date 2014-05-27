c3a373df8e76a870dec481ac77cdd8b3334cfd46
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-05-27 11:10:27 +0200

    initial square drawing and example page

--- js/chessground.js
+++ js/chessground.js
@@ -1,10 +1,20 @@
-
-'use strict';
-
 (function() {
+  'use strict';
 
   function Chessground(element, cfg) {
     // constructor
+
+    var drawSquares = function() {
+      var html = '';
+      var files = 'abcdefgh';
+      for (var rank = 8; rank > 0; rank--) {
+        for (var file = 1; file < 9; file++) {
+          var key = files[file - 1] + rank;
+          html += '<div data-key="' + key + '"></div>';
+        }
+      }
+      element.innerHTML = html;
+    };
   }
 
   window.Chessground = Chessground;
