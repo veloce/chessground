b801a27c40ff04641267e1fe9b4ef2075f03c9a5
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-05-27 11:57:11 +0200

    include chess.min.js and draw FEN pieces

--- js/chessground.js
+++ js/chessground.js
@@ -3,11 +3,10 @@
 
   function Chessground(element, cfg) {
     // constructor
-    console.log(element);
+    var files = 'abcdefgh';
 
     var drawSquares = function() {
       var html = '';
-      var files = 'abcdefgh';
       for (var rank = 8; rank > 0; rank--) {
         for (var file = 1; file < 9; file++) {
           var key = files[file - 1] + rank;
@@ -16,7 +15,19 @@
       }
       element.innerHTML = html;
     };
+
+    var drawPieces = function(chess) {
+      Array.prototype.forEach.call(element.children, function(square) {
+        var piece = chess.get(square.getAttribute('data-key'));
+        var html = piece ? '<div class="piece ' + piece.type + ' ' + piece.color + '"></div>' : '';
+        square.innerHTML = html;
+      });
+    };
+
     drawSquares();
+    var fen = 'rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3';
+    var chess = new Chess(fen);
+    drawPieces(chess);
   }
 
   window.Chessground = Chessground;
