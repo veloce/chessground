f80b0e5a7079368be96a7c8ee4d2d205223cfbf5
Vincent Velociter <vincent.velociter@gmail.com>
2014-07-14 20:34:12 +0200

    First implem of dragging square

--- src/drag.js
+++ src/drag.js
@@ -3,6 +3,43 @@
 var interact = require('./vendor/interact');
 var ctrl = require('./ctrl');
 
+var draggingDiv;
+
+document.addEventListener("DOMContentLoaded", function() {
+  draggingDiv = document.createElement('div');
+  draggingDiv.className = "moving-square";
+  document.body.appendChild(draggingDiv);
+}, false);
+
+function isHidden(el) {
+  return (el.offsetParent === null);
+}
+
+function getScrollXY () {
+  return {
+    x: window.scrollX || document.documentElement.scrollLeft,
+    y: window.scrollY || document.documentElement.scrollTop
+  };
+}
+
+function getElementRect (element) {
+  var scroll = /ipad|iphone|ipod/i.test(window.navigator.userAgent)
+  ? { x: 0, y: 0 }
+  : getScrollXY(),
+  clientRect = (element instanceof window.SVGElement)?
+  element.getBoundingClientRect():
+  element.getClientRects()[0];
+
+  return clientRect && {
+    left  : clientRect.left   + scroll.x,
+    right : clientRect.right  + scroll.x,
+    top   : clientRect.top    + scroll.y,
+    bottom: clientRect.bottom + scroll.y,
+    width : clientRect.width || clientRect.right - clientRect.left,
+    height: clientRect.heigh || clientRect.bottom - clientRect.top
+  };
+}
+
 function makeDraggable(root, state) {
   var transformProp = 'transform' in document.body.style?
   'transform': 'webkitTransform' in document.body.style?
@@ -11,10 +48,37 @@ function makeDraggable(root, state) {
   'oTransform': 'msTransform';
 
   interact('.square').dropzone(true)
+  .on('dragenter', function (event) {
+    var el = event.target;
+    var rect = getElementRect(el);
+    var h = rect.height - 1;
+    var w = rect.width - 1;
+    var h2 = h * 2;
+    var w2 = w * 2;
+
+    if (!draggingDiv.style.height) {
+      draggingDiv.style.height = h2 + 'px';
+      draggingDiv.style.width = w2 + 'px';
+    }
+
+    draggingDiv.style.left = rect.left - (w / 2);
+    draggingDiv.style.top = rect.top - (h / 2);
+
+    if (isHidden(draggingDiv)) {
+      draggingDiv.style.display = 'block';
+    }
+
+  })
+  .on('dragleave', function (event) {
+  })
+  .on('dragend', function() {
+  })
   .on('drop', function (event) {
+    draggingDiv.style.display = 'none';
     var piece = event.relatedTarget;
     var square = event.target;
     ctrl.movePiece(root, state, piece, square, true);
+
   });
 
   interact('.piece').draggable({
